f2d3989989cde8f5b75e3e9015b3997d32f75486
Utkarsh Ashish <utkarshashish@Utkarshs-MacBook-Pro.local>
2021-02-22 11:29:59 +0530

    added spring-web deps

--- src/main/java/com/example/springsession/SpringSessionApplication.java
+++ src/main/java/com/example/springsession/SpringSessionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSessionApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Utkarsh");
 		SpringApplication.run(SpringSessionApplication.class, args);
 	}
 
